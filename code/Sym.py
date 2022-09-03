cab3e53aea376ab3fa1f8ab68cb061789b35c46b
Rahul RK <rrangar@ncsu.edu>
2022-09-03 14:22:56 -0400

    Sym init add

--- code/Sym.py
+++ code/Sym.py
@@ -1,9 +1,14 @@
 class Sym:
-	def __init__(self):
-		pass
+	def __init__(self, c, s):
+		self.n = 0
+		self.at = c if c else 0
+		self.name = s if s else ""
+		self._has = {}
 
-	def add(self):
-		pass
+	def add(self, v):
+		if v!="?":
+			self.n = self.n+1
+			self._has[v] = 1 + ((self._has[v]) if self._has[v] else 0)
 
 	def mid(self):
 		pass
