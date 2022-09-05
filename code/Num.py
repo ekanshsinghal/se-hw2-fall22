c2508061149986be867d0cbc6cdfd91ce296dfdd
Ekansh Singhal <ekanshsinghal@icloud.com>
2022-09-05 14:49:35 -0400

    added Num methods

--- code/Num.py
+++ code/Num.py
@@ -1,13 +1,12 @@
 from cmath import inf
-import math
+from math import floor
 from collections import OrderedDict
-
-from attr import NOTHING
+from random import random
 
 the = {}
 
-def per(self, t=None, p=None):
-	p = math.floor(((p if p else 0.5) * len(t)) + 0.5)
+def per(t, p = None):
+	p = floor(((p if p else 0.5) * len(t)) + 0.5)
 	return t[max(1, min(len(t), p))]
 
 
@@ -23,20 +22,34 @@ class Num:
 		self.w= -1 if (s if s else '').endswith('-') else 1
 
 	def nums(self):
-		if not (self.isSorted): 
+		if not self.isSorted:
 			self._has = OrderedDict(sorted(self._has.items()))
 			self.isSorted = True
 		return self._has
 
 
-	def add(self, v=None, pos = None):
+	def add(self, v = None, pos = None):
+		if v != '?':
+			self.n += 1
+			self.lo = min(v, self.lo)
+			self.hi = max(v, self.hi)
+			if len(self._has) < the['nums']:
+				pos = len(self._has)
+			elif random() < (the['nums'] / self.n):
+				pos = random(len(self._has))
+			if pos:
+				self.isSorted = False
+				self._has[pos] = float(v)
+	
+	def mid(self):
+		return per(self.nums(), 0.5)
+
+	def div(self, a = None):
+		a = self.nums()
+		return (per(a, 0.9) - per(a, 0.1)) / 2.58
+
+
+	def test_Num():
 		pass
 
-	def mid(self, a=None):
-		return per(self.nums,0.5)
-
-	def div(self):
-		a = self.nums
-		return (per(a, 0.9)-per(a,0.1))/2.58
-
-	
+	test_Num()
