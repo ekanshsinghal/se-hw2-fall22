130485d57dcfa28381fcbd4f5cdcb6d885ee943c
Rahul RK <rrangar@ncsu.edu>
2022-09-12 04:15:52 -0400

    removed o and oo

--- code/Num.py
+++ code/Num.py
@@ -60,23 +60,6 @@ def cli(t):
 		v = str(v)
 		# Implement other modules to finish this.
 
-def o(t, show=None, u=None):
-	if not isinstance(t, dict):
-		return str(t)
-	def show(k, v):
-		if not str(k).find("\0"):
-			v = o(v)
-			return len(t)==0 and (":"+k+" "+v) or str(v)
-	u = {}
-	for k, v in t.items():
-		u[1+len(u)] = show(k,v)
-	if len(t) == 0:
-		u.sort()
-	return u
-
-def oo(t):
-	print(o(t))
-
 # def test_Num():
 # 	num = Num()
 # 	for i in range(1, 101):
