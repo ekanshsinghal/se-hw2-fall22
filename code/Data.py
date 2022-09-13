faa36c9d94944f0cdf4adb5b160e650f598270be
Rahul RK <rahulrk.2303@gmail.com>
2022-09-13 15:18:08 -0400

    Update Data.py

--- code/Data.py
+++ code/Data.py
@@ -12,27 +12,27 @@ class Data:
                 self.add(row)
             csv(src, add_rows) # Check row
         else:
-            for _, row in src.items():
+            for row in src:
                 self.add(row)
 
     def add(self, xs=None, row=None):
         if not self.cols:
-            self.cols = code.Cols.Cols(xs)
+            self.cols = Cols(xs)
         else:
-            row = self.rows.append(xs.cells and xs or Row(xs))
-            for _, todo in enumerate([self.cols.x, self.cols.y]):
+            row = push(self.rows, xs.cells and xs or Row(xs))
+            for _, todo in enumerate(self.cols.x + self.cols.y):
                 for _, col in todo.items():
-                    col.add(row.cells[col]) # Check what is col.at
+                    col.add(row.cells[col.at]) # Check what is col.at
 
-    def stats(self, places, showCols, fun, t, v):
+    def stats(self, places=3, showCols=None, fun=None, t=None, v=None):
         showCols = showCols or self.cols.y
         fun = fun or "mid"
 
         t = {}
 
-        for _, col in showCols.items():
+        for col in showCols:
             v = fun(col)
-            v = isinstance(v, int) and round(v, places) or v
+            v = isinstance(v, int) and rnd(v, places) or v
             t[col.name] = v
 
         return t
