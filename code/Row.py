ac1c10ad2dc689c39f3bb5d87b22fee536cd36fc
Rahul RK <rahulrk.2303@gmail.com>
2022-09-29 19:50:58 -0400

    Update Row.py

--- code/Row.py
+++ code/Row.py
@@ -1,9 +1,9 @@
-import copy
+# import copy
 
 class Row:
     def __init__(self, t=None):
         self.cells = t
-        self.cooked = copy.copy(t)
+        self.cooked = self.copy(t)
         self.isEvaled = False
 
     def copy(self, t, u=None):
