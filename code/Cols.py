28b88fcc83f8e8f198b0e12e8ed93f435d73cb44
Rahul RK <rahulrk.2303@gmail.com>
2022-09-30 02:49:27 -0400

    fix origin values cols

--- code/Cols.py
+++ code/Cols.py
@@ -9,17 +9,16 @@ class Cols:
 		self.klass = None
 		self.x = []
 		self.y = []
-		# regex = re.compile(r'^[A-Z]*')
 		for c, s in enumerate(names):
-			if type(s) == str and re.search("^[A-Z]", s):
+			if type(s) == str and s[0].isupper():
 				self.all.append(Num(the, c, s))
 			else:
 				self.all.append(Sym(c, s))
 			
-			if type(s) == str and not re.search(":$", s):
-				if '!' in s or '+' in s or '-' in s:
+			if type(s) == str and s[-1] != ':':
+				if '+' in s or '-' in s:
 					self.y.append(self.all[c])
 				else:
 					self.x.append(self.all[c])
-				if re.search('!$', s):
+				if s[-1] == '!':
 					self.klass = self.all[c]
